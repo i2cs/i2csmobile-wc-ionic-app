c13fefdc5cb7e2f700c70d33042e09bdd01ad653
Isuru Dilshan <isurudilsh@gmail.com>
2018-01-28 19:43:52 +0530

    Added customizations

--- www/app/variables.js
+++ www/app/variables.js
@@ -2,20 +2,17 @@ angular.module('starter')
     //.constant('BASE_URL', 'http://localhost/wordpress')
     //.constant('BASE_API_URL', 'http://localhost/wordpress/wp-json/i2cs/v1')
 
-    .constant('BASE_URL', 'http://saasthara.com/i2cs/shops/woocommerce/')
-    .constant('BASE_API_URL', 'http://saasthara.com/i2cs/shops/woocommerce/wp-json/i2cs/v1')
+    .constant('BASE_URL', 'https://saasthara.com/i2cs/shops/woocommerce/')
+    .constant('BASE_API_URL', 'https://www.9aydaly.com/wp-json/i2cs/v1')
 
-    .constant('WEBSITE', 'http://saasthara.com/i2cs/shops/woocommerce/')
-    .constant('FORGOT_LINK', 'http://saasthara.com/i2cs/shops/woocommerce/my-account/lost-password/')
-    .constant('EMAIL', 'i2cssolutions@gmail.com')
-    .constant('PHONE', '0712966650')
+    .constant('WEBSITE', 'https://www.9aydaly.com')
+    .constant('FORGOT_LINK', 'https://saasthara.com/i2cs/shops/woocommerce/my-account/lost-password/')
+    .constant('EMAIL', 'info@9aydaly.com')
+    .constant('PHONE', '+96599368686')
     .constant('ANALYTICS_ID', 'UA-79548648-1')
     .constant('COUPONS_ENABLED', true)
     .constant('STATUSBAR_COLOR', "#387ef5")
 	.constant('LANGUAGES', [
-            { name: "English", language_id: "en-US" },
-            { name: "French", language_id: "fr-FR" },
-            { name: "Chinese", language_id: "zh-CN" },
 	        { name: "Arabic", language_id: "ar-EG" }])
     .constant('WELCOME_SLIDES', [
             {
@@ -51,7 +48,7 @@ angular.module('starter')
     .constant('MAIN_OFFERS_BANNER_ID', 103)
     .constant('OFFERS_PAGE_BANNER_ID', 103)
     .constant('INTERCOM_INTEGRATION', true) // please check www/app/common/services/intercom.service.js
-    .constant('ONESIGNAL_APP_ID', "9302072d-783a-41f4-b3dc-80359f64ef96") // get OneSignal app id from http://onesignal.com
+    .constant('ONESIGNAL_APP_ID', "9302072d-783a-41f4-b3dc-80359f64ef96") // get OneSignal app id from https://onesignal.com
     .constant('GCM_SENDER_ID', "1037766790597")
     .constant('MENU_LAYOUT', "tabs")
 
